395fd37536869f32bbb8c52608d004f1076724d9
quack <kubaice3@gmail.com>
2021-01-20 14:58:46 +0100

    Started creating tests of ControllerEntry.

--- src/test/java/com/example/toucan_remake/entry/ControllerEntryTest.java
+++ src/test/java/com/example/toucan_remake/entry/ControllerEntryTest.java
@@ -1,12 +1,24 @@
 package com.example.toucan_remake.entry;
 
-import com.example.toucan_remake.ToucanRemakeApplication;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Test class for {@link ControllerEntry}.
@@ -14,12 +26,35 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ControllerEntry.class)
-@ContextConfiguration(classes= ToucanRemakeApplication.class)
 @AutoConfigureMockMvc
 public class ControllerEntryTest {
 
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void printApplicationContext() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(name -> applicationContext.getBean(name).getClass().getName())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
     @Test
-    public void  () {
+    public void getLandingPage_JWTOK_returnsDashboard() throws Exception {
+
+        File dashboard = new ClassPathResource("templates/dashboard.html").getFile();
+        String html = new String(Files.readAllBytes(dashboard.toPath()));
+
+        mockMvc.
+                perform(get(""))
+                .andExpect(status().isOk())
+                .andExpect(content().string(html))
+                .andDo(print());
+    }
+
+    @Test
+    public void getLandingPage_JWTNotOK_returnsLandingPage() {
 
     }
 
